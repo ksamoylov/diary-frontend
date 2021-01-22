ce3ac7287ce7eb09eadb1b04d2864babfc9600c4
ksamoylov <ksamoylov@team.amocrm.com>
2021-01-23 02:16:38 +0300

    Added little logic with navbar

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from "react-native-web";
+
+export default function Calendar() {
+  return (
+    <Text>Calendar</Text>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Navbar() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Diary</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      backgroundColor: '#f4f4f4',
+      height: 70,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingTop: 30,
+    },
+    text: {
+      fontSize: 16,
+    },
+  },
+);
